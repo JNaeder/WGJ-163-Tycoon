0a5cf0130c871fa7d8a2dcb3cf5ef57f7bf73125
JNaeder <j.naeder324@gmail.com>
2020-08-24 09:06:56 -0400

    das

---
+++ WGJ-163 Tycoon/Assets/ManagmentButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagmentButton : MonoBehaviour
+{
+
+    ArtistManagment aM;
+
+    public Artist buttonArtist;
+    public Producer buttonProducer;
+    public string buttonGenre;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aM = FindObjectOfType<ArtistManagment>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ArtistDetails() {
+        aM.ShowArtistDetails(buttonArtist);
+
+    }
+}
